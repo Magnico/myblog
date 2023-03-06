024dc467aa344205bf679b5fbfc246a072a06175
Magnico <ejmrebolledo@gmail.com>
2023-03-06 16:46:53 -0500

    Creation of post model and hello world

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100,null=False,blank=False)
+    body = models.TextField(max_length=255,null=False,blank=False)
+    author = models.ForeignKey(User,on_delete=models.CASCADE, null=False)
+
+    def __str__(self):
+        return self.title
