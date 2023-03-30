4b4af96584290f98f8d9650507ecb42cefdcb943
Magnico <ejmrebolledo@gmail.com>
2023-03-30 11:42:57 -0500

    Serializers for Comment model created

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -1,11 +1,28 @@
 from rest_framework import serializers
-from blog.models import Post
+from blog.models import Post , Comment
 
 
 class PostSerializer(serializers.ModelSerializer):
-
     #user string related fields
     author = serializers.StringRelatedField()
     class Meta:
         model = Post
         fields = ['title','body','author']
+
+class RelatedPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['title','pk']
+
+class CommentSerializer(serializers.ModelSerializer):
+    #user string related fields
+    author = serializers.StringRelatedField()
+    post = RelatedPostSerializer()
+    class Meta:
+        model = Comment
+        fields = ['body','author','post']
+
+class CommentPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['body','author','post']
