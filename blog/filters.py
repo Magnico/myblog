6d8ae7db958afc7d5a10b8358bdcdcbb4a033249
Magnico <ejmrebolledo@gmail.com>
2023-04-20 12:52:50 -0500

    Added order and search options

--- blog/filters.py
+++ blog/filters.py
@@ -4,6 +4,12 @@ from blog.models import Post
 class PostFilter(filters.FilterSet):
     user = filters.CharFilter(field_name='author__username', lookup_expr='icontains')
     safe = filters.BooleanFilter(field_name='safe')
+    orderings = filters.OrderingFilter(
+        fields=(
+            ('title', 'title'),
+            ('safe', 'safe'),
+        )
+    )
 
     class Meta:
         model = Post
