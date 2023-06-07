6d0bd6e24536854fcb796e02d7157e60114a2e1b
Magnico <ejmrebolledo@gmail.com>
2023-06-07 16:09:43 -0500

    Name fix

--- myblog/settings.py
+++ myblog/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'blog.middleware.RedisMiddleware',
+    'blog.middleware.RedisVisitMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
